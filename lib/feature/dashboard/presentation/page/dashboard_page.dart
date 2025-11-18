26456e59620228addb8c23ac986dcccedffeeccc
jubayer040 <62088928+jbjason@users.noreply.github.com>
2025-11-18 12:33:56 +0600

    Update dashboard_page.dart

--- lib/feature/dashboard/presentation/page/dashboard_page.dart
+++ lib/feature/dashboard/presentation/page/dashboard_page.dart
@@ -1,9 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hello_bazar/core/util/my_dimens.dart';
+
+enum LogLevel { info, warning, error, debug }
+
+class Clash {
+  final String name;
+  final LogLevel level;
+  const Clash({required this.name, required this.level});
+}
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
+
+  void logMessage(String message, LogLevel level) => print(message);
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +21,22 @@ class DashboardPage extends StatelessWidget {
       appBar: MyDimens().getNormalAppBar("Dashboard", [], context),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsetsGeometry.symmetric(
-            horizontal: 12.w,
-            vertical: 10.h,
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    
-                  },
-                  child: Text("Dashboard Page"),
-                ),
-              ],
-            ),
+          padding: const.all(10),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  final item = Clash(name: "jb", level: LogLevel.warning);
+                  logMessage("Log Msg 1 !", .warning);
+                  if (item.level == .error) print("conditionz Error");
+                },
+                child: Text("Submit"),
+              ),
+              Stack(fit: .expand, children: []),
+              Container(constraints: .expand()),
+              Container(decoration: BoxDecoration(shape: .circle)),
+            ],
           ),
         ),
       ),
